0b7301bcbf780b41f3fa93bec46a12876ebce58c
Prajwal Bati <prajwalbati@gmail.com>
2023-04-15 22:48:13 +0545

    load more added

--- src/beer/action/index.ts
+++ src/beer/action/index.ts
@@ -5,7 +5,7 @@ export const getAllBeers = createAsyncThunk<any, any>(
   'get/beers',
   async ({ page, perPage }) => {
     try {
-      let apiUrl = `https://api.punkapi.com/v2/beers`;
+      let apiUrl = `https://api.punkapi.com/v2/beers?page=${page}&per_page=${perPage}`;
       const response = await axios.get(apiUrl);
       if (response.status === 200) {
         return response.data;
